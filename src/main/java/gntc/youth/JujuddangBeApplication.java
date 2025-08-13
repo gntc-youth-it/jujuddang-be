2bf697d140da8773c73bb44586ab621d3a7901f1
Seokhee Park <seokhee_park@nexcubecorp.com>
2025-08-13 11:01:56 +0900

    init: 프로젝트 생성

---
+++ src/main/java/gntc/youth/JujuddangBeApplication.java
@@ -0,0 +1,13 @@
+package gntc.youth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JujuddangBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JujuddangBeApplication.class, args);
+    }
+
+}
